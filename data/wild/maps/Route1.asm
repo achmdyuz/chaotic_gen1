2c4718f52ba90f6c34ecaedf7b0a5c994f19090a
achmdyuz <achm4d.bat@gmail.com>
2026-06-27 15:57:13 +0700

    MewTwo, Mew, Zapdos, Articuno, Moltres on Route-1

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, PIDGEY
-	db  3, RATTATA
-	db  3, RATTATA
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, PIDGEY
-	db  3, PIDGEY
-	db  4, RATTATA
-	db  4, PIDGEY
-	db  5, PIDGEY
+	db  3, MEWTWO
+	db  3, MEW
+	db  3, ZAPDOS
+	db  2, MOLTRES
+	db  2, MEWTWO
+	db  3, MEWTWO
+	db  3, MEWTWO
+	db  4, ARTICUNO
+	db  4, MEWTWO
+	db  5, MEWTWO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
